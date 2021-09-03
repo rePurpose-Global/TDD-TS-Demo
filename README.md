5993c2f9f11fa461daec914ea2a5e876e2f6aaaa
kpguru20001 <kpguru20001@gmail.com>
2021-09-03 09:14:06 +0000

    Fixed the no. bug
    Co-authored-by: Joel Louzado <jLouzado@users.noreply.github.com>
    Co-authored-by: Shrey Ambesh <ShreyAmbesh@users.noreply.github.com>

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 [ ] Matches no.
 [ ] If you win exits
 [ ] if you loose ask if you want to try again
+
+## Bugs after v1
+
+1. User is able to enter a guess outside the scope
+2. User is able to enter a number as their name
+   - However, even Elon Musk's child should be to play.
--- index.ts
+++ index.ts
@@ -7,6 +7,10 @@ let shouldContinue = true;
 
 do {
   const guess = questionInt("What no. is on your mind b/w 1-10?\n");
+  if(guess>10 || guess<1){
+      console.error('Please read the instructions properly');
+      continue;
+  }
   const answer = Math.floor(Math.random() * 10 + 1);
   shouldContinue = guess !== answer ;
   console.log(`The correct answer was ${answer}\n`);
